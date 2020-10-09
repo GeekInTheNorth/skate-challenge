23c4f32e2c88d885af2065ef500ac0288e6c86ab
Mark Stott <mypantsareontheoutside@gmail.com>
2020-10-09 17:53:50 +0100

    Initial creation of the Strava Webhook Controller

---
+++ src/AllInSkateChallenge.Features/Strava/Webhook/StravaWebhookController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+namespace AllInSkateChallenge.Features.Strava.Webhook
+{
+    public class StravaWebhookController : Controller
+    {
+        public IActionResult ProcessEvent(WebHookEvent webHookEvent)
+        {
+            return Ok();
+        }
+    }
+
+    public class WebHookEvent 
+    { 
+        [JsonProperty("aspect_type")]
+        public string AspectType { get; set; }
+
+        [JsonProperty("event_time")]
+        public DateTime EventTime { get; set; }
+
+        [JsonProperty("object_id")]
+        public string ObjectId { get; set; }
+
+        [JsonProperty("object_type")]
+        public string ObjectType { get; set; }
+
+        [JsonProperty("owner_id")]
+        public string OwnerId { get; set; }
+
+        [JsonProperty("subscription_id")]
+        public string SubscriptionId { get; set; }
+
+        [JsonProperty("updates")]
+        public Dictionary<string, string> Updates { get; set; }
+    }
+}
