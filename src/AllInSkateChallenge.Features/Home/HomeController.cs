59d2383278aa841afeb6ac594b1a4a3b7d8d28fd
Mark Stott <mypantsareontheoutside@gmail.com>
2020-10-11 23:13:24 +0100

    Realign menus and add placeholder for FAQs

--- src/AllInSkateChallenge.Features/Home/HomeController.cs
+++ src/AllInSkateChallenge.Features/Home/HomeController.cs
@@ -34,6 +34,11 @@ namespace AllInSkateChallenge.Features.Home
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
