6591245df41227eda3c254dffc5f616085ea9a0b
Mark Stott <mypantsareontheoutside@gmail.com>
2020-10-19 00:27:14 +0100

    Add Migrations for Strava Events table.

--- src/AllInSkateChallenge.Features/Data/Migrations/20201018232325_AssignEventsToUsers.Designer.cs
+++ src/AllInSkateChallenge.Features/Data/Migrations/20201018232325_AssignEventsToUsers.Designer.cs
@@ -4,14 +4,16 @@ using AllInSkateChallenge.Features.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AllInSkateChallenge.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201018232325_AssignEventsToUsers")]
+    partial class AssignEventsToUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AllInSkateChallenge.Features/Data/Migrations/20201018232325_AssignEventsToUsers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AllInSkateChallenge.Data.Migrations
+{
+    public partial class AssignEventsToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StravaEvents",
+                columns: table => new
+                {
+                    StravaEventId = table.Column<Guid>(nullable: false),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    StravaActivityId = table.Column<string>(nullable: true),
+                    Imported = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StravaEvents", x => x.StravaEventId);
+                    table.ForeignKey(
+                        name: "FK_StravaEvents_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StravaEvents_ApplicationUserId_StravaActivityId",
+                table: "StravaEvents",
+                columns: new[] { "ApplicationUserId", "StravaActivityId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StravaEvents");
+        }
+    }
+}
