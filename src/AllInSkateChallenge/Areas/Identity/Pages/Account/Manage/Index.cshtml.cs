150dfd00bb5b388db88c4f762f37b7cabb00385c
Mark Stott <mypantsareontheoutside@gmail.com>
2020-12-01 22:17:44 +0000

    Add the ability for users to change their personal target.

--- src/AllInSkateChallenge/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/AllInSkateChallenge/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 using AllInSkateChallenge.Features.Data.Entities;
+using AllInSkateChallenge.Features.Data.Static;
+using AllInSkateChallenge.Features.Gravatar;
 
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AllInSkateChallenge.Areas.Identity.Pages.Account.Manage
 {
@@ -13,13 +17,19 @@ namespace AllInSkateChallenge.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ICheckPointRepository _checkPointRepository;
+        private readonly IGravatarResolver _gravatarResolver;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            ICheckPointRepository checkPointRepository, 
+            IGravatarResolver gravatarResolver)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _checkPointRepository = checkPointRepository;
+            _gravatarResolver = gravatarResolver;
         }
 
         public string Username { get; set; }
@@ -30,11 +40,19 @@ namespace AllInSkateChallenge.Areas.Identity.Pages.Account.Manage
         [BindProperty]
         public InputModel Input { get; set; }
 
+        public IList<SelectListItem> SkateTargets => _checkPointRepository.GetSelectList();
+
         public class InputModel
         {
+            [Display(Name = "Gravatar")]
+            public string GravatarUrl { get; set; }
+
             [Display(Name = "Display Name")]
             [Required]
             public string SkaterName { get; set; }
+
+            [Display(Name = "Your Personal Target")]
+            public SkateTarget Target { get; set; }
 
             [Phone]
             [Display(Name = "Phone number")]
@@ -53,9 +71,11 @@ namespace AllInSkateChallenge.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
+                GravatarUrl = _gravatarResolver.GetGravatarUrl(user.Email),
                 PhoneNumber = phoneNumber,
                 SkaterName = user.SkaterName,
-                AcceptProgressNotifications = user.AcceptProgressNotifications
+                AcceptProgressNotifications = user.AcceptProgressNotifications,
+                Target = user.Target
             };
         }
 
@@ -87,12 +107,14 @@ namespace AllInSkateChallenge.Areas.Identity.Pages.Account.Manage
 
             var phoneNumberChanged = !string.Equals(Input.PhoneNumber, user.PhoneNumber);
             var skaterNameChanged = !string.Equals(Input.SkaterName, user.SkaterName);
+            var targetChanged = !Input.Target.Equals(user.Target);
             var acceptProgressChanged = !Input.AcceptProgressNotifications.Equals(user.AcceptProgressNotifications);
-            if (phoneNumberChanged || skaterNameChanged || acceptProgressChanged)
+            if (phoneNumberChanged || skaterNameChanged || acceptProgressChanged || targetChanged)
             {
                 user.PhoneNumber = Input.PhoneNumber;
                 user.SkaterName = Input.SkaterName;
                 user.AcceptProgressNotifications = Input.AcceptProgressNotifications;
+                user.Target = Input.Target;
 
                 var saveResult = await _userManager.UpdateAsync(user);
                 if (!saveResult.Succeeded)
