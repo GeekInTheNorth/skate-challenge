3b8ba17712bebe5a8f04693818ba9273793d2042
Mark Stott <mypantsareontheoutside@gmail.com>
2021-01-14 23:37:32 +0000

    Update Skate Log Entry form to prevent invalid date entry and update styles

--- src/AllInSkateChallenge.Features/Skater/SkateLog/SkaterLogViewModel.Form.cs
+++ src/AllInSkateChallenge.Features/Skater/SkateLog/SkaterLogViewModel.Form.cs
@@ -3,7 +3,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AllInSkateChallenge.Features.Skater.SkateLog
 {
-
     public partial class SkaterLogViewModel : INewSkaterLogEntry
     {
         public string JourneyName { get; set; }
