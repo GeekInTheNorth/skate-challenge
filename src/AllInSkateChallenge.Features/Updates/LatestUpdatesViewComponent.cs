bd0ea7ec6b33f12c087d46f18563925726f7de65
Mark Stott <mypantsareontheoutside@gmail.com>
2021-02-07 01:13:08 +0000

    Create initial view component for the latest updates feature.

---
+++ src/AllInSkateChallenge.Features/Updates/LatestUpdatesViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllInSkateChallenge.Features.Updates
+{
+    public class LatestUpdatesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int take, int skip)
+        {
+            var model = new LatestUpdatesViewComponentModel
+            {
+                Take = take,
+                Skip = skip,
+                LatestUpdatesUrl = "https://allinskatechallengefunctions.azurewebsites.net/api/SkaterLog/List/"
+            };
+
+            return View("~/Views/Shared/Components/LatestUpdates/Default.cshtml", model);
+        }
+    }
+}
---
+++ src/AllInSkateChallenge.Features/Updates/LatestUpdatesViewComponentModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AllInSkateChallenge.Features.Updates
+{
+    public class LatestUpdatesViewComponentModel
+    {
+        public int Take { get; set; }
+
+        public int Skip { get; set; }
+
+        public string LatestUpdatesUrl { get; set; }
+    }
+}
