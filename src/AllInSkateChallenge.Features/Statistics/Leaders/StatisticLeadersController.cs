0f1c84fac744898daf99a0e96f6efbcfaf9e8c68
Mark Stott <mypantsareontheoutside@gmail.com>
2021-06-11 23:10:35 +0100

    Add links to the event statistics page.

---
+++ src/AllInSkateChallenge.Features/Statistics/Leaders/StatisticLeadersController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using AllInSkateChallenge.Features.Data.Entities;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllInSkateChallenge.Features.Statistics.Leaders
+{
+    [Authorize]
+    public class StatisticLeadersController : Controller
+    {
+        private readonly IStatisticLeadersViewModelBuilder viewModelBuilder;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public StatisticLeadersController(IStatisticLeadersViewModelBuilder viewModelBuilder, UserManager<ApplicationUser> userManager)
+        {
+            this.viewModelBuilder = viewModelBuilder;
+            this.userManager = userManager;
+        }
+
+        [Route("EventStatistics/Leaders/{statisticType}")]
+        public async Task<IActionResult> Index(StatisticType statisticType)
+        {
+            var userDetails = await userManager.GetUserAsync(User);
+            var model = await viewModelBuilder.WithStatisticType(statisticType).WithUser(userDetails).Build();
+
+            return View(model);
+        }
+    }
+}
--- src/AllInSkateChallenge.Features/Statistics/Leaders/StatisticLeadersViewModelBuilder.cs
+++ src/AllInSkateChallenge.Features/Statistics/Leaders/StatisticLeadersViewModelBuilder.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-using AllInSkateChallenge.Features.Data.Entities;
 using AllInSkateChallenge.Features.Framework.Models;
 
 using MediatR;
-
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 
 namespace AllInSkateChallenge.Features.Statistics.Leaders
 {
@@ -66,29 +61,6 @@ namespace AllInSkateChallenge.Features.Statistics.Leaders
         {
             this.statisticType = statisticType;
             return this;
-        }
-    }
-
-    [Authorize]
-    public class StatisticLeadersController : Controller
-    {
-        private readonly IStatisticLeadersViewModelBuilder viewModelBuilder;
-
-        private readonly UserManager<ApplicationUser> userManager;
-
-        public StatisticLeadersController(IStatisticLeadersViewModelBuilder viewModelBuilder, UserManager<ApplicationUser> userManager)
-        {
-            this.viewModelBuilder = viewModelBuilder;
-            this.userManager = userManager;
-        }
-
-        [Route("EventStatistics/Leaders/{statisticType}")]
-        public async Task<IActionResult> Index(StatisticType statisticType)
-        {
-            var userDetails = await userManager.GetUserAsync(User);
-            var model = await viewModelBuilder.WithStatisticType(statisticType).WithUser(userDetails).Build();
-
-            return View(model);
         }
     }
 }
