b125bd26bfb7ea257ae0d0a77d2e647dbc158670
Mark Stott <mypantsareontheoutside@gmail.com>
2023-05-25 23:43:18 +0100

    Re-enable Content Security Policy

--- src/AllInSkateChallenge/Startup.cs
+++ src/AllInSkateChallenge/Startup.cs
@@ -156,13 +156,13 @@ namespace AllInSkateChallenge
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.SameOrigin());
             app.UseReferrerPolicy(opt => opt.NoReferrerWhenDowngrade());
-            //app.UseCsp(opt => opt.DefaultSources(s => s.Self())
-            //                     .ScriptSources(s => s.Self().UnsafeInline().UnsafeEval().CustomSources("https://ajax.aspnetcdn.com", "https://cdn.jsdelivr.net", "https://unpkg.com/vue@2.6.12/dist/vue.js"))
-            //                     .StyleSources(s => s.Self().UnsafeInline())
-            //                     .ConnectSources(s => s.Self().CustomSources("https://all-in-skate-challenge-legacy-functions.azurewebsites.net"))
-            //                     .FrameSources(s => s.None())
-            //                     .FrameAncestors(s => s.None())
-            //                     .ImageSources(s => s.Self().CustomSources("data:", "https:", "https://www.gravatar.com")));
+            app.UseCsp(opt => opt.DefaultSources(s => s.Self())
+                                 .ScriptSources(s => s.Self().UnsafeInline().UnsafeEval().CustomSources("https://ajax.aspnetcdn.com", "https://cdn.jsdelivr.net", "https://unpkg.com/vue@2.6.12/dist/vue.js"))
+                                 .StyleSources(s => s.Self().UnsafeInline())
+                                 .ConnectSources(s => s.Self().CustomSources("https://rggskatemarathonfunctions.azurewebsites.net"))
+                                 .FrameSources(s => s.None())
+                                 .FrameAncestors(s => s.None())
+                                 .ImageSources(s => s.Self().CustomSources("data:", "https:", "https://www.gravatar.com")));
 
             app.UseHttpsRedirection();
 
