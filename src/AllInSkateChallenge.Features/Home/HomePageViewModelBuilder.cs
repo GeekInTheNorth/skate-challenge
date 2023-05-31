b682f101245c14f9f1e6174d52ecd82b9c6a8c14
Mark Stott <mypantsareontheoutside@gmail.com>
2023-05-31 14:52:44 +0100

    Update HomePageViewModelBuilder.cs

--- src/AllInSkateChallenge.Features/Home/HomePageViewModelBuilder.cs
+++ src/AllInSkateChallenge.Features/Home/HomePageViewModelBuilder.cs
@@ -20,7 +20,7 @@ namespace AllInSkateChallenge.Features.Home
             var eventStatistics = await mediator.Send(new EventSummaryQuery());
             var model = await base.Build();
             model.PageTitle = "Home";
-            model.DisplayPageTitle = "Welcome to the Roller Girl Gang Leeds Skate Challenge";
+            model.DisplayPageTitle = "Welcome to the Roller Girl Gang Virtual Skate Marathon";
             model.IsNoIndexPage = false;
             model.Content.NumberOfSkaters = eventStatistics.NumberOfSkaters;
             model.Content.CumulativeMiles = eventStatistics.CumulativeMiles;
