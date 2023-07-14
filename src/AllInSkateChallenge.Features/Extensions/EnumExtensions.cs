3a227518f159ea5b3c0522dbaf17acdb221f0601
Mark Stott <mypantsareontheoutside@gmail.com>
2023-07-15 00:00:56 +0100

    Add the user's skate target to the administrator screens.

--- src/AllInSkateChallenge.Features/Extensions/EnumExtensions.cs
+++ src/AllInSkateChallenge.Features/Extensions/EnumExtensions.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿namespace AllInSkateChallenge.Features.Extensions;
+
+using System;
 using System.Linq;
 
-namespace AllInSkateChallenge.Features.Extensions
-{
-    public static class EnumExtensions
-    {
-        public static TEnum Next<TEnum>(this TEnum value) where TEnum : struct
-        {
-            return Enum.GetValues(value.GetType()).Cast<TEnum>().Concat(new[] { default(TEnum) }).SkipWhile(e => !value.Equals(e)).Skip(1).First();
-        }
+using Humanizer;
 
-        public static TEnum Previous<TEnum>(this TEnum value) where TEnum : struct
-        {
-            return Enum.GetValues(value.GetType()).Cast<TEnum>().Concat(new[] { default(TEnum) }).Reverse().SkipWhile(e => !value.Equals(e)).Skip(1).First();
-        }
+public static class EnumExtensions
+{
+    public static TEnum Next<TEnum>(this TEnum value) where TEnum : struct
+    {
+        return Enum.GetValues(value.GetType()).Cast<TEnum>().Concat(new[] { default(TEnum) }).SkipWhile(e => !value.Equals(e)).Skip(1).First();
+    }
+
+    public static TEnum Previous<TEnum>(this TEnum value) where TEnum : struct
+    {
+        return Enum.GetValues(value.GetType()).Cast<TEnum>().Concat(new[] { default(TEnum) }).Reverse().SkipWhile(e => !value.Equals(e)).Skip(1).First();
+    }
+
+    public static string GetDisplayName<TEnum>(this TEnum value) where TEnum : struct
+    {
+        return value.ToString().Humanize().Transform(To.TitleCase);
     }
 }
