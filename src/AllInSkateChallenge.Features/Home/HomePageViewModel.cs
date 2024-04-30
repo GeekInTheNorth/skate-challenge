9e598392c78f4127c318be42cd969686566c3e40
Mark Stott <mypantsareontheoutside@gmail.com>
2024-04-30 10:58:09 +0100

    Hide the All Leaderboards and All Update buttons temporarily.

--- src/AllInSkateChallenge.Features/Home/HomePageViewModel.cs
+++ src/AllInSkateChallenge.Features/Home/HomePageViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using AllInSkateChallenge.Features.Common;
 
-namespace AllInSkateChallenge.Features.Home
-{
-    public class HomePageViewModel
-    {
-        public int NumberOfSkaters { get; set; }
-        
-        public decimal CumulativeMiles { get; set; }
+namespace AllInSkateChallenge.Features.Home;
 
-        public decimal CumulativeKilometers => Conversion.MilesToKilometres(CumulativeMiles);
-    }
+public class HomePageViewModel
+{
+    public int NumberOfSkaters { get; set; }
+    
+    public decimal CumulativeMiles { get; set; }
+
+    public decimal CumulativeKilometers => Conversion.MilesToKilometres(CumulativeMiles);
+
+    public bool ShowLeaderBoardButton { get; set; }
+
+    public bool ShowAllUpdatesButton { get; set; }
 }
--- src/AllInSkateChallenge.Features/Home/HomePageViewModelBuilder.cs
+++ src/AllInSkateChallenge.Features/Home/HomePageViewModelBuilder.cs
@@ -1,31 +1,26 @@
 ﻿using System.Threading.Tasks;
-
 using AllInSkateChallenge.Features.Framework.Models;
 
 using MediatR;
 
-namespace AllInSkateChallenge.Features.Home
+namespace AllInSkateChallenge.Features.Home;
+
+public class HomePageViewModelBuilder(IMediator mediator) : PageViewModelBuilder<HomePageViewModel>(mediator), IHomePageViewModelBuilder
 {
-    public class HomePageViewModelBuilder : PageViewModelBuilder<HomePageViewModel>, IHomePageViewModelBuilder
+    private readonly IMediator mediator = mediator;
+    
+    public override async Task<PageViewModel<HomePageViewModel>> Build()
     {
-        private readonly IMediator mediator;
+        var eventStatistics = await mediator.Send(new EventSummaryQuery());
+        var model = await base.Build();
+        model.PageTitle = "Home";
+        model.DisplayPageTitle = "Welcome to the Roller Girl Gang Virtual Skate Marathon";
+        model.IsNoIndexPage = false;
+        model.Content.NumberOfSkaters = eventStatistics.NumberOfSkaters;
+        model.Content.CumulativeMiles = eventStatistics.CumulativeMiles;
+        model.Content.ShowLeaderBoardButton = false;
+        model.Content.ShowAllUpdatesButton = false;
 
-        public HomePageViewModelBuilder(IMediator mediator) : base(mediator)
-        {
-            this.mediator = mediator;
-        }
-
-        public override async Task<PageViewModel<HomePageViewModel>> Build()
-        {
-            var eventStatistics = await mediator.Send(new EventSummaryQuery());
-            var model = await base.Build();
-            model.PageTitle = "Home";
-            model.DisplayPageTitle = "Welcome to the Roller Girl Gang Virtual Skate Marathon";
-            model.IsNoIndexPage = false;
-            model.Content.NumberOfSkaters = eventStatistics.NumberOfSkaters;
-            model.Content.CumulativeMiles = eventStatistics.CumulativeMiles;
-
-            return model;
-        }
+        return model;
     }
 }
